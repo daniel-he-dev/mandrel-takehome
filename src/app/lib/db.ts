e5f35b6040a84f2a3ddd0e1aac6adda8b5b3d064
Daniel He <danielhe.mlp@gmail.com>
2024-11-20 18:55:19 -0500

    enable ssl

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -7,7 +7,6 @@ const pool = new Pool({
   host: process.env.POSTGRES_HOST ?? "localhost",
   port: Number(process.env.POSTGRES_PORT) ?? 5432,
   database: process.env.POSTGRES_DB,
-  ssl: false,
 });
 
 export const query = (text: string, params?: string[]) =>
